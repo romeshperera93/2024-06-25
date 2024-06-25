14e50a8e81322a299458dcee78e56f5ac16c3264
romeshperera93 <romeshperera123@gmail.com>
2024-06-25 22:33:17 +0530

    Fourth commit

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ console.log(customer.name);
 console.log(customer.age);
 console.log(customer.address);
 console.log(customer.phone);
+
+
+const number=[1,2,3,4,5,6,7,8,9]
+
+console.log(number[2]);
+
+for (let i=0;i<number.length;i++){
+    console.log("Hello");
+}
