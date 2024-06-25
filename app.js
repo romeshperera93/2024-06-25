99ed4127e30bccbe3c7d14e28eba5441d7e22cfb
romeshperera93 <romeshperera123@gmail.com>
2024-06-25 22:17:29 +0530

    Third commit

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ let isThisEqual = (5=='5');
 
 console.log(isEqual);
 console.log(isThisEqual);
+
+const customer ={
+    name:"saman",
+    age:12,
+    address:"Gampaha",
+    phone:"077234587"
+}
+console.log(customer.name);
+console.log(customer.age);
+console.log(customer.address);
+console.log(customer.phone);
